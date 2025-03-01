46ebe540d69abd15b71dc582c410d7209034ccc5
root <root@sangshin.42.fr>
2025-03-01 16:21:13 +0900

    add .env and add .env to gitignore

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -5,8 +5,8 @@ chmod +x wp-cli.phar
 php -d memory_limit=512M wp-cli.phar core download --allow-root
 
 #./wp-cli.phar core download --allow-root
-./wp-cli.phar config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
-./wp-cli.phar core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
-./wp-cli.phar user create user2 user2@example.com --user_pass=user2password --role=subscriber --allow-root
+./wp-cli.phar config create --dbname=wordpress --dbuser=wpuser --dbpass=${WP_PASSWD} --dbhost=mariadb --allow-root
+./wp-cli.phar core install --url=localhost --title=inception --admin_user=${ADMIN_USER} --admin_password=${ADMIN_PASSWD} --admin_email=admin@admin.com --allow-root
+./wp-cli.phar user create user2 user2@example.com --user_pass=${USER2_PASSWD} --role=subscriber --allow-root
 
 php-fpm83 -F
